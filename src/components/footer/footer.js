7d56ff1177f2fe63f36b689d394fb5a4c02ce45a
aliensoft-web <oripovd1@gmail.com>
2020-01-19 21:28:05 +0500

    change font

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,44 +2,35 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './footer.css';
 import telegram from '../../images/telegram-brands.svg';
-import rss from '../../images/rss-solid.svg';
+import fb from '../../images/facebook-brands.svg';
+import youtube from '../../images/youtube-brands.svg';
 
 const Footer = () => (
     <footer className="site-footer">
         <div className="site-info">
-            <div className="footer-aside">
-                <div className="pk-subscribe-form-wrap">
-                    <div className="pk-subscribe-container">
-                        <div className="pk-subscribe-data">
-                            <p className="pk-subscribe-message">Подписывайся на рассылку и раз в неделю получай лучшие статьи на почту</p>
-                            <form className="subscription">
-                                <input type="hidden" name="list_id"/>
-                                <div className="pk-input-group">
-                                    <input type="text" name="email" className="email form-control" placeholder="Ваш e-mail" />
-                                    <button className="pk-subscripe-submit">Подписаться</button>
-                                </div>
-                                <input type="hidden" name="_wp_http_referer" />
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="footer-content">
+            <div className="footer-content content-area">
                 <h5 className="site-title footer-title">Blog</h5>
                 <div className="pk-social-links-template-nav">
                 <div className="pk-social-links-items">
                     <div className="pk-social-links-item">
-                        <a href="https://t.me/" className="pk-social-links-link" target="_blank" rel="noopener noreferrer">
+                        <a href="https://t.me/iqtisodchi_kundaligi" className="pk-social-links-link" target="_blank" rel="noopener noreferrer">
                             <i className="pk-social-links-icon pk-icon pk-icon-telegram">
                                 <img src={telegram} alt="Telegram"/>
                             </i>
-                            <span className="pk-social-links-count">4K</span>
+                            <span className="pk-social-links-count">9K</span>
                         </a>
                     </div>
                     <div className="pk-social-links-item">
-                        <a href="https://t.me/" className="pk-social-links-link" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.youtube.com/channel/UCRsqjw8SVM9oMZPP0anA_kA" className="pk-social-links-link" target="_blank" rel="noopener noreferrer">
                             <i className="pk-social-links-icon pk-icon pk-icon-rss">
-                                <img src={rss} alt="RSS"/>
+                                <img src={youtube} alt="YouTube"/>
+                            </i>
+                        </a>
+                    </div>
+                    <div className="pk-social-links-item">
+                        <a href="https://t.me/iqtisodchi_kundaligi" className="pk-social-links-link" target="_blank" rel="noopener noreferrer">
+                            <i className="pk-social-links-icon pk-icon pk-icon-rss">
+                                <img src={fb} alt="Facebook"/>
                             </i>
                         </a>
                     </div>
