98758e4f361aba68846932f846a01e8d7660b087
aliensoft-web <oripovd1@gmail.com>
2020-02-08 18:42:21 +0500

    change content

--- src/pages/episodes.js
+++ src/pages/episodes.js
@@ -28,6 +28,7 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
             image
+            name
           }
           fields {
             slug
